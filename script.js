1e8919b9c766c6c4a5e625d8f2424b953e521a3b
rohith <rohith@rohiths-Air.lan>
2025-10-07 20:42:01 +0530

    added login page

--- script.js
+++ script.js
@@ -7,4 +7,21 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // prevent form reload
 
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const error = document.getElementById("error");
+
+    // Example credentials
+    const validUser = "admin";
+    const validPass = "12345";
+
+    if (username === validUser && password === validPass) {
+        alert("Welcome to Gayathri Enterprises ");
+        window.location.href = "index.html"; // redirect to home page
+    } else {
+        error.textContent = "Invalid username or password!";
+    }
+});
